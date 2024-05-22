918f6b1cec14ada7cb1748d4aa5703bcb39b4d1c
Isay Robles <isay.robles@semovioaxaca.gob.mx>
2024-05-22 10:07:58 -0600

    Actualizacion del main.py, al añadir un tercer parametro para ordenar la lista, asi como modificacion de la linea de comando en el makefile y finalmente agregar un archivo de ejemplo con palabras.txt

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
 
 
-def sort_list(items, ascending=True):
+def sort_list(items, ascending):
     if not isinstance(items, list):
         raise RuntimeError(f"No puede ordenar {type(items)}")
 
@@ -24,12 +24,14 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
+        sort_lists = sys.argv[3].lower() == "ascendente"
     else:
         print("Se debe indicar el fichero como primer argumento")
         print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("El tercer argumento indica si se quiere ordenar la lista ascendente o descendente")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
@@ -46,4 +48,4 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    print(sort_list(word_list, sort_lists))
